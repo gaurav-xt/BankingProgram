9f75cbd746592254023fb0aceed11f9d6170eed3
gaurav <gauravtambare7@gmail.com>
2026-01-01 15:42:32 +0530

    Banking Program In Java

---
+++ src/Main.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+
+
+        double balance = 0;
+        boolean isRunning = true;
+        int choice;
+
+        while (isRunning){
+
+            System.out.println("------------------");
+            System.out.println("BANKING PROGRAM");
+            System.out.println("------------------");
+            System.out.println("1. Show balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.println("-----------------");
+
+            System.out.print("Enter your choice (1-4): ");
+            choice = scanner.nextInt();
+
+            switch (choice){
+                case 1 -> showBalance(balance);
+                case 2 -> balance =balance + deposit();
+                case 3 -> balance = balance - withdraw(balance );
+                case 4 -> isRunning = false;
+                default -> System.out.println("INVALID CHOICE");
+            }
+        }
+
+
+        System.out.println("------------------------------");
+        System.out.println("Thank you! Have a nice day!");
+        System.out.println("-------------------------------");
+
+
+        scanner.close();
+
+
+    }
+    static void showBalance(double balance){
+
+        System.out.println("-----------------");
+        System.out.printf("$%.2f\n", balance);
+
+    }
+    static double deposit(){
+
+        double amount;
+
+        System.out.print("Enter an amount to deposit: ");
+        amount = scanner.nextDouble();
+
+        if(amount < 0){
+            System.out.println("Amount can't be negative");
+            return 0;
+
+        }else {
+           return amount;
+        }
+    }
+    static double withdraw(double balance){
+
+        double amount;
+
+        System.out.print("Enter amount to be withdrawn: ");
+        amount = scanner.nextDouble();
+
+        if(amount > balance){
+            System.out.println("Insufficient funds");
+            return 0;
+        } else if (amount < 0) {
+            System.out.println("Amount can't be negative");
+            return 0;
+
+        }else {
+            return amount;
+        }
+
+
+    }
+}
+
